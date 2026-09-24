81cc8b4a96622a0e8050ef40e107d349004b89b3
monochromehamburger <2598787@learn.vsb.bc.ca>
2026-09-24 14:56:09 -0700

    Add files via upload

--- MultAdd.java
+++ MultAdd.java
@@ -1,8 +1,15 @@
 class MultAdd{
 	public static void main(String args[]){
-		multadd(6,7,8);
+		expSum(2);
+		System.out.println("𓇼 ⋆.˚ 𓆉 𓆝 𓆡⋆.˚ 𓇼");
+		System.out.println(multadd(Math.log(10),1,Math.log(20)));
 	}
-	public static void multadd(int a, int b, int c){
-		System.out.println(a*b+c);
+	public static double multadd(double a, double b, double c){
+		return a*b+c;
 	}
-}
+	public static void expSum(double a){
+		double output=multadd(a,Math.exp(-1*a),Math.sqrt(1-Math.exp(-1*a)));
+		System.out.println(output);
+	}
+}                                                                                           
+                        
